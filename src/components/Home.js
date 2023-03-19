a343cac7178827e9d7ff85c82d3b837e4a5e84bd
vinit-churi <vinitchuri@hotmail.com>
2023-03-19 13:12:49 +0530

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 const Home = () => {
-
     return (
-        <div id='home-page'>
-
+        <div id="home-page">
             <h1>Home page</h1>
             <h2>Hi, my name is Siddharth </h2>
-            <h2>and I work at <a href="https://www.newtonschool.co/">Newton School</a></h2>
-           <button id="home-to-blog"> Go to Blog</button>
-            <button id="home-to-info"> Go to Info</button>
-
+            <h2>
+                and I work at{" "}
+                <a href="https://www.newtonschool.co/">Newton School</a>
+            </h2>
+            <Link to="/blog">
+                <button id="home-to-blog"> Go to Blog</button>
+            </Link>
+            <Link to="/info">
+                <button id="home-to-info"> Go to Info</button>
+            </Link>
         </div>
-    )
-}
-export default Home
+    );
+};
+export default Home;
--- src/components/Info.js
+++ src/components/Info.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Info = () => {
-
-
     return (
         <div id="info-page">
-
-
             <h1> Info page </h1>
             <h2>I am born in India</h2>
             <h2>and I like to play Cricket </h2>
-             <button id="info-to-blog"> Go to Blog</button>
-            <button id="info-to-home"> Go to Home</button>
+            <Link to="/blog">
+                <button id="info-to-blog"> Go to Blog</button>
+            </Link>
+            <Link to="/">
+                <button id="info-to-home"> Go to Home</button>
+            </Link>
         </div>
-    )
-}
-export default Info
+    );
+};
+export default Info;
